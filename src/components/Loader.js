bc7a3cb8c9c970c084206d37a6b89a526a27228b
ayan4m1 <andrew@bulletlogic.com>
2023-07-10 20:54:17 -0400

    make loader look pretty

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,3 +1,12 @@
 export default function Loader() {
-  return <h1>Loading...</h1>;
+  return (
+    <div className="d-flex w-100 h-100 justify-content-center align-items-center bg-secondary">
+      <h1
+        className="display-1 text-light"
+        style={{ textShadow: '5px 4px 4px #262323' }}
+      >
+        Loading...
+      </h1>
+    </div>
+  );
 }
