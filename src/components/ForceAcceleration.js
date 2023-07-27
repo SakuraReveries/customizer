d66c5c8bc70b074758c2ecdda5237496b220ebcb
ayan4m1 <andrew@bulletlogic.com>
2023-07-27 00:16:40 -0400

    add device detection to add instructions on accel off and bypass for CPU-only users

--- src/components/ForceAcceleration.js
+++ src/components/ForceAcceleration.js
@@ -1,24 +1,64 @@
 import PropTypes from 'prop-types';
 import { useDetectGPU } from '@react-three/drei';
+import { isChrome, isChromium, isEdge, isFirefox } from 'react-device-detect';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-export default function ForceOrientation({ children }) {
+export default function ForceAcceleration({ children }) {
+  const [bypassed, setBypassed] = useState(false);
   const gpu = useDetectGPU({
     failIfMajorPerformanceCaveat: true
   });
 
-  if (gpu?.tier > 0) {
+  if (gpu?.tier > 0 || bypassed) {
     return children;
   } else {
     return (
-      <div className="d-flex w-100 h-100 justify-content-center align-items-center bg-secondary">
-        <h1 className="display-3 text-light sr-loader-heading">
-          Enable Hardware Acceleration
+      <div className="d-flex flex-column w-100 h-100 justify-content-center bg-secondary">
+        <h1 className="display-4 text-center text-light">
+          It looks like hardware acceleration is disabled.
         </h1>
+        <Container fluid>
+          <Row>
+            <Col xs={12} className="text-center">
+              {(isChrome || isChromium || isEdge) && (
+                <p className="text-light">
+                  Go to Settings &gt; System and check &quot;Use hardware
+                  acceleration when available.&quot;
+                </p>
+              )}
+              {isFirefox && (
+                <Row>
+                  <Col xs={12} className="text-center">
+                    <p className="text-light">
+                      Go to Settings &gt; General &gt; Performance and uncheck
+                      &quot;Use Recommended Performance settings.&quot; Then
+                      uncheck the &quot;Use hardware acceleration when
+                      available&quot; checkbox.
+                    </p>
+                  </Col>
+                </Row>
+              )}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} className="text-center">
+              <p>
+                <Button variant="danger" onClick={() => setBypassed(true)}>
+                  <FontAwesomeIcon icon={faExclamationTriangle} fixedWidth />{' '}
+                  Load Anyway (Here Be Dragons)
+                </Button>
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
 
-ForceOrientation.propTypes = {
+ForceAcceleration.propTypes = {
   children: PropTypes.node.isRequired
 };
